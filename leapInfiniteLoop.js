00c73cc9d51e56c9cabe31964d982d753e45ccd2
alexdowns0 <alexdowns0@gmail.com>
2020-09-07 13:44:56 -0400

    stored hand information based on one hand line 57

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -7,15 +7,22 @@ var y = 680;
 
 Leap.loop(controllerOptions, function(frame) 
 {
-	clear();
-	circle(x, y, 25);
-	randNumX = (Math.random() * 2) - 1;
-	randNumY = (Math.random() * 2) - 1;
-	x += randNumX;
-	y += randNumY;
+	// clear();
+	// circle(x, y, 25);
+	// randNumX = (Math.random() * 2) - 1;
+	// randNumY = (Math.random() * 2) - 1;
+	// x += randNumX;
+	// y += randNumY;
 	
+	if(frame.hands.length == 1){
+		var hand = console.log(frame.hands[0]);
+	}
 	
 
-
+	// if (frame.length > 0)
+	// {
+	// 	console.log(frame.hands);
+	// }
+	
 }
 );
