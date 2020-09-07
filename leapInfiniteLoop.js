32a488f47623e5adefac26dbe0a614fb157b6e35
alexdowns0 <alexdowns0@gmail.com>
2020-09-07 19:20:04 -0400

    created HandleFrame function line 62

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,8 +5,7 @@ var x = 1272;
 // browser height
 var y = 680;
 
-Leap.loop(controllerOptions, function(frame) 
-{
+function HandleFrame(frame){
 	// clear();
 	// circle(x, y, 25);
 	// randNumX = (Math.random() * 2) - 1;
@@ -15,14 +14,29 @@ Leap.loop(controllerOptions, function(frame)
 	// y += randNumY;
 	
 	if(frame.hands.length == 1){
-		var hand = console.log(frame.hands[0]);
-	}
+		
+		var hands = frame.hands[0];
+		var fingers = hands.fingers;
 	
 
-	// if (frame.length > 0)
-	// {
-	// 	console.log(frame.hands);
-	// }
-	
+		var arrayLength = fingers.length;
+
+		for (var i = 0; i < arrayLength; i++){
+			current = fingers[i];
+			if(current.type == 1)
+			{
+		    	return console.log(current);
+			}
+		}
+
+		
+	}
 }
+Leap.loop(controllerOptions, function(frame) 
+{
+			
+	HandleFrame(frame);
+}
+	
+	
 );
