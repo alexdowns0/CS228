0a59574d5a5bbf7d562f969cadd866a3e5f4c080
alexdowns0 <alexdowns0@gmail.com>
2020-09-07 20:55:59 -0400

    attempt to scale x and y values

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,9 +1,19 @@
 var controllerOptions = {};
 
-// browswer width
-var x = 1272;
-// browser height
-var y = 680;
+
+
+var rawXMin = 2000;
+var rawXMax = -2000;
+var rawYMin = 2000;
+var rawYMax = -2000;
+var newXMin = 0;
+var newXMax = window.innerWidth;
+var newXVal = ((rawXMin - rawXMax)) / (rawXMax - rawXMax) * (100 - 0);
+var newYMin = 0;
+var newYVal = ((rawYMin - rawYMax)) / (rawYMax - rawYMax) * (100 - 0);
+var newYMax = window.innerHeight;
+
+
 
 function HandleFrame(frame)
 {
@@ -39,15 +49,46 @@ function HandleFinger(finger)
 			current = finger[i];
 			if(current.type == 1)
 			{
-		    	return console.log(current);
+		    	//console.log(current.tipPosition);
+		    	var x = current.tipPosition[0];
+		    	var y = current.tipPosition[1];
+		    	var z = current.tipPosition[2];
+
+		    	if(x < rawXMin)
+		    	{
+		    		x == rawXMin;
+		    	}
+
+		    	if(x > rawXMax)
+		    	{
+		    		x == rawXMax;
+		    	}
+
+		    	if(y < rawYMin)
+		    	{
+		    		y == rawYMin;
+		    	}
+
+		    	if(y > rawYMax)
+		    	{
+		    		y == rawYMax;
+		    	}
+
+		    	y = window.innerHeight - y;
+		    	return circle(x, y, 25);
+
 			}
 		}
+
 }
 
 Leap.loop(controllerOptions, function(frame) 
 {
-			
+	clear();		
 	HandleFrame(frame);
+	
+
+
 	
 }
 	
