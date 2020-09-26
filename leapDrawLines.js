4de355851500f1151bc21ae93b17fb1f6a0b1176
Alex Downs <alexandra.downs@uvm.edu>
2020-09-26 18:30:09 -0400

    finished del02, display clean lines to indicate four bones in five fingers with color, also cleaned up and commented code

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -1,10 +1,6 @@
-
-
-
 var controllerOptions = {};
 
-
-
+// variables for window sizing 
 var rawXMin = 10000;
 var rawXMax = -10000;
 var rawYMin = 10000;
@@ -13,9 +9,11 @@ var newXMax = window.innerWidth;
 var newXMin = 0;
 var newYMax = window.innerHeight;
 var newYMin = 0;
+
+// tracker variable to count loop iterations 
 var b = 0;
 
-
+// HandleFrame function, returns first hand that is within frame 
 function HandleFrame(frame)
 {
 	if(frame.hands.length == 1)
@@ -25,99 +23,141 @@ function HandleFrame(frame)
 	}
 }
 
+// HandleHand function, returns fingers and bones within the hand variable
 function HandleHand(hand)
 {	
 	// var for each finger from hand 
 	var fingers = hand.fingers;
-	
-	for (boneIndex = 3; boneIndex >= 0; boneIndex--)
+
+	// for loop that iterates through each bone (4 total)
+	for (var boneIndex = 3; boneIndex >= 0; boneIndex--)
 	{
+		// for loop that iterates through each finger (5 total)
 		for(var fingerIndex = 0; fingerIndex < 5; fingerIndex++)
 		{
-			// only one bone that is being called
-			bone = fingers[fingerIndex].bones[boneIndex];
-			HandleBone(bone);
-			//console.log(b, fingerBone);
-			//console.log(boneIndex, fingerIndex, bone);
-			
-		}	
+			// only one bone that is being stored in bone variable
+			var bone = fingers[fingerIndex].bones;
+
+			//console.log(bone, bone.type);
+
+			// variable for strokeWeight
+			var strokeWidth = 4;
+
+			// call handlebone function passing three variables
+			HandleBone(boneIndex, bone, strokeWidth);
+
+		}
 	}		
 }
 
+// HandleFinger function removed in Del 02 
 // function HandleFinger(fingers)
 //  {	    		
 	
 //  }
 
-
-function TransformCoordinates (xb, yb)
+// TransformCoordinates function, manipulates hand on screen to make sure it isn't inverted and fits well 
+function TransformCoordinates (xt, yt)
 {
-
-	yb = window.innerHeight - yb;
-		    	
-	xb = (((xb - rawXMin) * (newXMax - newXMin)) / (rawXMax - rawXMin)) + newXMin;
-		    	
-	yb = (((yb - rawYMin) * (newYMax - newYMin)) / (rawYMax - rawYMin)) + newYMin;
-
-	return [xb,yb];
-}
-
-function HandleBone(bone)
-{
-	var boneType = ["metacarpal", "proximal phalange", "intermediate phalange", "distal phalange"];
-
-	var boneI = bone.type
-	//var currentFingerBone = boneType[boneI];
-	var bonePreviousJ = bone.prevJoint;
-	var boneNextJ = bone.nextJoint;
-	//console.log(b, bonePreviousJ, boneNextJ);
-	
-
-	var xb = boneNextJ[0];
-	var yb = boneNextJ[1];
-	var zb = boneNextJ[2];
-	[xb,yb] = TransformCoordinates(xb,yb);
-	var xt = bonePreviousJ[0];
-	var yt = bonePreviousJ[1];
-	var zt = bonePreviousJ[2];
-	[xt,yt] = TransformCoordinates(xt,yt);
-
-	//console.log(b, x1, y1, z1);
-
-	if(boneNextJ[0] < rawXMin)
+	if(xt < rawXMin)
 	{
-		rawXMin = boneNextJ[0]; 
+		rawXMin = xt; 
 		    		
 	}
 
-	if(boneNextJ[0] > rawXMax)
+	if(xt > rawXMax)
 	{
-		rawXMax = boneNextJ[0]; 
+		rawXMax = xt; 
 	}
 
-	if(boneNextJ[1] < rawYMin)
+	if(yt < rawYMin)
 	{
-		rawYMin = boneNextJ[1];
+		rawYMin = yt;
 	}
 
-	if(boneNextJ[1] > rawYMax)
+	if(yt > rawYMax)
 	{
-		rawYMax = boneNextJ[1];
+		rawYMax = yt;
 	}
-
-	// yt = window.innerHeight - y1;
-		    	
-	// xt = (((x1 - rawXMin) * (newXMax - newXMin)) / (rawXMax - rawXMin)) + newXMin;
-		    	
-	// yt = (((y1 - rawYMin) * (newYMax - newYMin)) / (rawYMax - rawYMin)) + newYMin;
-
 	
-	console.log(xb,yb);
-	line(xb,yb,xt,yt);
-	//return circle(x1, y1, 25);
+	
+	var newX = (((xt - rawXMin) * (newXMax - newXMin)) / (rawXMax - rawXMin)) + newXMin;	    	
+	var newY = (((yt - rawYMin) * (newYMax - newYMin)) / (rawYMax - rawYMin)) + newYMin;
+
+	newY = window.innerHeight - yt;	  	
+
+	return[newX,newY];
 
 }
-				
+
+// HandleBone function, function to display lines indicating each bone
+function HandleBone(boneIndex, bone, strokeWidth)
+{
+	// variables for base of bone x, y, and z; tip of bone x, y, and z
+	var xb = bone[boneIndex].nextJoint[0];
+	var yb = bone[boneIndex].nextJoint[1];
+	var zb = bone[boneIndex].nextJoint[2];
+	var xt = bone[boneIndex].prevJoint[0];
+	var yt = bone[boneIndex].prevJoint[1];
+	var zt = bone[boneIndex].prevJoint[2];
+
+	var newB = TransformCoordinates(xb,yb);
+	var newBX = newB[0];
+	var newBY = newB[1];
+	var newT = TransformCoordinates(xt,yt);
+	var newTX = newT[0];
+	var newTY = newT[1];
+
+	// variables to store color vals
+	var r = 0;
+	var g = 0;
+	var b = 0;
+	
+	// if statements to indicate darker bones are bones father from hand
+	if(bone[boneIndex].type == 0)
+	{
+		strokeWidth = 10;
+		
+		r = 189;
+		g = 218;
+		b = 219;
+	}
+
+	else if(bone[boneIndex].type == 1)
+	{
+		strokeWidth = 8;
+	
+		r = 134;
+		b = 174;
+		g = 176;
+	}
+
+	else if(bone[boneIndex].type == 2)
+	{
+		strokeWidth = 5;
+		r = 118;
+		g = 151;
+		b = 153;
+	}
+
+	else if(bone[boneIndex].type == 3)
+	{
+		strokeWidth = 5;
+		r = 79;
+		b = 115;
+		g = 117;
+	}
+
+	// give hand color and width
+	stroke(r,g,b);
+	strokeWeight(strokeWidth);
+
+	// draw those hand lines
+	line(newBX,newBY,newTX, newTY);
+
+}
+
+// the infinite loop				
 Leap.loop(controllerOptions, function(frame) 
 {
 	b+=1;
@@ -126,4 +166,3 @@ Leap.loop(controllerOptions, function(frame)
 }
 		
 );
-
