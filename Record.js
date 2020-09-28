7bf63564fa708f7fd64af6d93e56e1dba1d11b5b
Alex Downs <alexandra.downs@uvm.edu>
2020-09-28 13:39:20 -0400

    del 03 line 29 created tensor

--- Record.js
+++ Record.js
@@ -1,5 +1,8 @@
 var controllerOptions = {};
 
+// create variable for one frame of data
+var oneFrameOfData = nj.zeros([5, 4, 6]);
+
 // variables for window sizing 
 var rawXMin = 10000;
 var rawXMax = -10000;
@@ -35,21 +38,22 @@ function HandleHand(hand, frame)
 	var fingers = hand.fingers;
 
 	// for loop that iterates through each bone (4 total)
-	for (var boneIndex = 3; boneIndex >= 0; boneIndex--)
+	for (var boneI = 3; boneI >= 0; boneI--)
 	{
 		// for loop that iterates through each finger (5 total)
-		for(var fingerIndex = 0; fingerIndex < 5; fingerIndex++)
+		for(var fingerI = 0; fingerI < 5; fingerI++)
 		{
 			// only one bone that is being stored in bone variable
-			var bone = fingers[fingerIndex].bones;
+			var bone = fingers[fingerI].bones;
 
 			//console.log(bone, bone.type);
 
 			// variable for strokeWeight
 			var strokeWidth = 4;
-
+			var fingerIndex = fingers[fingerI].type;
 			// call handlebone function passing three variables
-			HandleBone(boneIndex, bone, strokeWidth, frame);
+			var boneIndex = bone[boneI].type;
+			HandleBone(boneIndex,boneI, bone, strokeWidth, frame, fingerIndex);
 
 		}
 	}		
@@ -66,8 +70,7 @@ function TransformCoordinates (xt, yt)
 {
 	if(xt < rawXMin)
 	{
-		rawXMin = xt; 
-		    		
+		rawXMin = xt; 	    		
 	}
 
 	if(xt > rawXMax)
@@ -96,15 +99,15 @@ function TransformCoordinates (xt, yt)
 }
 
 // HandleBone function, function to display lines indicating each bone
-function HandleBone(boneIndex, bone, strokeWidth, frame)
+function HandleBone(boneI, boneIndex, bone, strokeWidth, frame, fingerIndex)
 {
 	// variables for base of bone x, y, and z; tip of bone x, y, and z
-	var xb = bone[boneIndex].nextJoint[0];
-	var yb = bone[boneIndex].nextJoint[1];
-	var zb = bone[boneIndex].nextJoint[2];
-	var xt = bone[boneIndex].prevJoint[0];
-	var yt = bone[boneIndex].prevJoint[1];
-	var zt = bone[boneIndex].prevJoint[2];
+	var xb = bone[boneI].nextJoint[0];
+	var yb = bone[boneI].nextJoint[1];
+	var zb = bone[boneI].nextJoint[2];
+	var xt = bone[boneI].prevJoint[0];
+	var yt = bone[boneI].prevJoint[1];
+	var zt = bone[boneI].prevJoint[2];
 
 	var newB = TransformCoordinates(xb,yb);
 	var newBX = newB[0];
@@ -112,6 +115,20 @@ function HandleBone(boneIndex, bone, strokeWidth, frame)
 	var newT = TransformCoordinates(xt,yt);
 	var newTX = newT[0];
 	var newTY = newT[1];
+
+	// sum of all x y and z for base and tip elements 0 -> 2
+	var sum = newBX + newBY + zb + newTX + newTY + zt;
+
+	// 120 element tensor
+	oneFrameOfData.set(fingerIndex, boneIndex, 0, newTX);
+	oneFrameOfData.set(fingerIndex, boneIndex, 1, newTY);
+	oneFrameOfData.set(fingerIndex, boneIndex, 2, zt);
+	oneFrameOfData.set(fingerIndex, boneIndex, 3, newBX);
+	oneFrameOfData.set(fingerIndex, boneIndex, 4, newBY);
+	oneFrameOfData.set(fingerIndex, boneIndex, 5, zb);
+	// oneFrameOfData.set(3, sum, newTX);
+	// oneFrameOfData.set(4, sum, newTY);
+	// oneFrameOfData.set(5, sum, zt);
 
 	// variables to store color vals
 	var r = 0;
@@ -122,40 +139,36 @@ function HandleBone(boneIndex, bone, strokeWidth, frame)
 	if(frame.hands.length == 1)
 	{
 		// if statements to indicate darker bones are bones father from hand
-		if(bone[boneIndex].type == 0)
+		if(boneIndex == 0)
 		{
 			strokeWidth = 10;
 			r = 9;
 			g = 232;
-			b = 69;
-			
+			b = 69;	
 		}
 
-		else if(bone[boneIndex].type == 1)
+		else if(boneIndex == 1)
 		{
 			strokeWidth = 8;
 			r = 6;
 			g = 161;
-			b = 48;
-			
+			b = 48;		
 		}
 
-		else if(bone[boneIndex].type == 2)
+		else if(boneIndex == 2)
 		{
 			strokeWidth = 5;
 			r = 4;
 			g = 110;
-			b = 32;
-			
+			b = 32;		
 		}
 
-		else if(bone[boneIndex].type == 3)
+		else if(boneIndex == 3)
 		{
 			strokeWidth = 5;
 			r = 2;
 			g = 31;
 			b = 10;
-
 		}
 	}
 
@@ -163,50 +176,43 @@ function HandleBone(boneIndex, bone, strokeWidth, frame)
 	else if(frame.hands.length == 2)
 	{
 		// if statements to indicate darker bones are bones father from hand
-		if(bone[boneIndex].type == 0)
+		if(boneIndex == 0)
 		{
 			strokeWidth = 10;
 			r = 224;
 			g = 12;
-			b = 4;
-			
+			b = 4;		
 		}
 
-		else if(bone[boneIndex].type == 1)
+		else if(boneIndex == 1)
 		{
 			strokeWidth = 8;
 			r = 173;
 			g = 12;
-			b = 7;
-			
+			b = 7;		
 		}
 
-		else if(bone[boneIndex].type == 2)
+		else if(boneIndex == 2)
 		{
 			strokeWidth = 5;
 			r = 115;
 			g = 9;
-			b = 6;
-
-			
+			b = 6;		
 		}
 
-		else if(bone[boneIndex].type == 3)
+		else if(boneIndex == 3)
 		{
 			strokeWidth = 5;
 			r = 51;
 			g = 3;
 			b = 2;
-
 		}
 	}
 	// give hand color and width
 	stroke(r,g,b);
 	strokeWeight(strokeWidth);
-
 	// draw those hand lines
 	line(newBX,newBY,newTX, newTY);
-
 }
 
 // RecordData Function to show when hands switch from 2 to 1 in frame 
@@ -215,7 +221,10 @@ function RecordData()
 	if(currentNumHands == 1 && previousNumHands == 2)
 	{
 		background('#222222');
+		// snapshot of when secondary hand leaves frame
+		console.log(oneFrameOfData.toString());
 	}
+
 }
 
 // the infinite loop				
@@ -226,8 +235,8 @@ Leap.loop(controllerOptions, function(frame)
 	HandleFrame(frame);
 	RecordData();	
 	previousNumHands = currentNumHands;
-	// b+=1;
-	 	
+	// this is for my own knowledge
+	b+=1;
 		
 }
 		
