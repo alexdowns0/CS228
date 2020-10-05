f0fe46721d4d5e400587d2431c3f428fa9267735
alexdowns0 <alexdowns0@gmail.com>
2020-10-05 19:37:52 -0400

    del 05 line 34

--- PredictGestures.js
+++ PredictGestures.js
@@ -26,24 +26,31 @@ function draw()
 		
 
 	// }
-	Test();	
-	Train();
 	
+	Train();
+	Test();	
 }
 
 // train function, go through all iris data and add to knnClassifier
 function Train()
 {
+	for (var tensorIterator = 0; tensorIterator < train0.shape[3]; tensorIterator++)
+	{
+		var features = train0.pick(null, null, null, tensorIterator);
+		console.log(features.toString());
 
-	trainingCompleted = true;
-	
+	}
+
+	//console.log("done");
+	//trainingCompleted = true;
+	//console.log(train0.toString());
 }
 
 // test function, test knn classifier
 function Test()
 {
 		
-	
+	//console.log("4");
 	
 }
 
