31f1182c2f85af76536c67c639ffff7aa54099b5
alexdowns0 <alexdowns0@gmail.com>
2020-10-05 21:34:09 -0400

    del 05 line 43 make predictions, changed around some variables, had issue with label

--- PredictGestures.js
+++ PredictGestures.js
@@ -1,18 +1,17 @@
 // init variables, constants, and irisData array
 const knnClassifier = ml5.KNNClassifier();
-var testingSampleIndex = 1;
-var currentFeatures = nj.array();
+var testingSampleIndex = 0;
+var currentFeatures;
 var numSamples = 0;
 nj.config.printThreshold = 1000;
-var Features = 0;
-
-var currentLabel = 0;
+var predictedLabel;
+var currentLabel;
 var trainingCompleted = false;
 
 
 
 
-var predictedClassLabels = nj.zeros([numSamples]);
+var predictedClassLabels = nj.zeros([1,test.shape[3]]);
 
 
 // draw function, draw circles if the machine learning training is finished
@@ -21,8 +20,8 @@ function draw()
 	//console.log(irisData.toString());
 	//console.log(numSamples,numFeatures);
 	clear();	
-	// if (trainingCompleted == false)
-	// {
+	//if (trainingCompleted == false)
+	//{
 		
 
 	// }
@@ -36,29 +35,53 @@ function Train()
 {
 	for (var tensorIterator = 0; tensorIterator < train0.shape[3]; tensorIterator++)
 	{
-		var features = train0.pick(null, null, null, tensorIterator).reshape(1, 120);
+		var features = train0.pick(null, null, null, tensorIterator);
+		features = features.reshape(120);
 		knnClassifier.addExample(features.tolist(), 0);
 		//console.log(features.toString());
 
 	}
 
 	//console.log("done");
-	//trainingCompleted = true;
+	trainingCompleted = true;
 	//console.log(train0.toString());
 }
 
 // test function, test knn classifier
 function Test()
 {
-	for (var tensorIterator = 0; tensorIterator < test.shape[3]; tensorIterator++)
-	{
-		var features2 = test.pick(null, null, null, tensorIterator).reshape(1, 120);
-		knnClassifier.addExample(features2.tolist(), 0);
-		console.log(features2.toString)
-	}
+	currentFeatures = test.pick(null, null, null, testingSampleIndex);
+	currentLabel = 0;
+	//currentLabel = 0;
+	predictedLabel = knnClassifier.classify(currentFeatures.tolist(),GotResults);
 	//console.log("4");
+
+	//console.log(currentFeatures);
 	
 }
+
+function GotResults(err, result)
+{
+	//predictedClassLabels.set(testingSampleIndex,parseInt(result.label));
+	
+	console.log(testingSampleIndex + " " + result.label);
+	
+	
+	if (testingSampleIndex >= test.shape[3])
+	{
+	  testingSampleIndex = 0;
+	}
+	else {
+	testingSampleIndex++;
+}
+//console.log(testingSampleIndex, test.shape[3]);
+	//console.log(currentTestingSample.toString());
+	
+	//console.log(testingSampleIndex);
+	
+
+}
+
 
 
 
