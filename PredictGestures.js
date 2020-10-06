da0c4c5a21601dce6015edbb40af76c6269e3453
alexdowns0 <alexdowns0@gmail.com>
2020-10-05 22:41:17 -0400

    del 05 line 57 add new test.js data, update sign for my numbers 2 and 3

--- PredictGestures.js
+++ PredictGestures.js
@@ -20,39 +20,33 @@ function draw()
 	//console.log(irisData.toString());
 	//console.log(numSamples,numFeatures);
 	clear();	
-	//if (trainingCompleted == false)
-	//{
-		
+	if (trainingCompleted == false)
+	{
+		Train();
 
-	// }
+	}
 	
-	Train();
+	
 	Test();	
 }
 
 // train function, go through all iris data and add to knnClassifier
 function Train()
 {
+	trainingCompleted = true;
 	for (var tensorIterator = 0; tensorIterator < train3.shape[3]; tensorIterator++)
 	{
 		var features = train3.pick(null, null, null, tensorIterator);
 		features = features.reshape(120);
-		knnClassifier.addExample(features.tolist(), 0);
+		knnClassifier.addExample(features.tolist(), 3);
+		features = train2.pick(null, null, null, tensorIterator);
+		features = features.reshape(120);
+		knnClassifier.addExample(features.tolist(), 2);
 		//console.log(features.toString());
-
-	}
-
-	for (var tensorIterator2 = 0; tensorIterator2 < train2.shape[3]; tensorIterator2++)
-	{
-		var features2 = train2.pick(null, null, null, tensorIterator2);
-		features2 = features2.reshape(120);
-		knnClassifier.addExample(features2.tolist(), 0);
-		//console.log(features.toString());
-
 	}
 
 	//console.log("done");
-	trainingCompleted = true;
+	
 	//console.log(train0.toString());
 }
 
@@ -60,8 +54,7 @@ function Train()
 function Test()
 {
 	currentFeatures = test.pick(null, null, null, testingSampleIndex);
-	currentLabel = 0;
-	//currentLabel = 0;
+	
 	predictedLabel = knnClassifier.classify(currentFeatures.tolist(),GotResults);
 	//console.log("4");
 
@@ -71,18 +64,17 @@ function Test()
 
 function GotResults(err, result)
 {
-	//predictedClassLabels.set(testingSampleIndex,parseInt(result.label));
+	predictedClassLabels.set(testingSampleIndex,parseInt(result.label));
 	
 	console.log(testingSampleIndex + " " + result.label);
 	
-	
+	testingSampleIndex++;
+
 	if (testingSampleIndex >= test.shape[3])
 	{
 	  testingSampleIndex = 0;
 	}
-	else {
-	testingSampleIndex++;
-}
+
 //console.log(testingSampleIndex, test.shape[3]);
 	//console.log(currentTestingSample.toString());
 	
