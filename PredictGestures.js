12f5c4c4552b213aa55afa85279eacad39d297a6
alexdowns0 <alexdowns0@gmail.com>
2020-11-01 13:16:18 -0500

    del 08 line 25

--- PredictGestures.js
+++ PredictGestures.js
@@ -37,8 +37,16 @@ var trainingCompleted = false;
 function SignIn()
 {
 	//console.log('Sign in');
-	username = document.getElementById(’username’).value;
-	console.log(username);
+	username = document.getElementById('username').value;
+	var list = document.getElementById('users');
+	var item = document.createElement('li');
+	//console.log(username);
+	item.innerHTML = String(username);
+   	list.appendChild(item);
+
+   console.log(list.innerHTML);
+
+   return false;
 }
 
 function DetermineState(frame)
