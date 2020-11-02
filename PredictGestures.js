542bd6c332057b79981b13667d3fab4290e77003
alexdowns0 <alexdowns0@gmail.com>
2020-11-02 18:48:34 -0500

    del 08 completed

--- PredictGestures.js
+++ PredictGestures.js
@@ -37,17 +37,23 @@ var trainingCompleted = false;
 function SignIn()
 {
 	//console.log('Sign in');
-	username = document.getElementById('username').value;
+	var username = document.getElementById('username').value;
 	var list = document.getElementById('users');
 	if (IsNewUser(username,list))
 	{
-		var item = document.createElement('li');
-		//console.log(username);
-		item.innerHTML = String(username);
-	   	list.appendChild(item);
-
-   		
+		CreateNewUser(username, list);
+		CreateSignInItem(username, list);
+		
+		//console.log(username);	
 	}
+
+	else
+	{
+		var ID = String(username) + "_signins";
+		var listItem = document.getElementById(ID);
+		listItem.innerHTML = parseInt(listItem.innerHTML) + 1;
+	}
+
 	console.log(list.innerHTML);
    return false;
 }
@@ -64,9 +70,25 @@ function IsNewUser(username, list)
 			usernameFound = true;
 		}
 		//console.log(users[i] + " " + users[i].innerHTML);
-
 	}
 	return usernameFound == false;
+}
+
+function CreateNewUser(username, list)
+{
+	var item = document.createElement('li');
+	item.id = String(username) + "_name";
+	item.innerHTML = String(username);
+	list.appendChild(item);
+
+}
+
+function CreateSignInItem(username, list)
+{
+	var itemB = document.createElement('li');
+	itemB.id = String(username) + "_signins";
+	itemB.innerHTML = 1;
+	list.appendChild(itemB);
 }
 
 function DetermineState(frame)
